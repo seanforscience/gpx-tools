5e220a17a3ac0d03f31c7e164915f2d4bbb5fbd3
Sean <sean@probablyridiculous.com>
2020-08-10 17:28:43 -0700

    complete refactor of heatmap.

--- src/heatmap.py
+++ src/heatmap.py
@@ -1,70 +1,46 @@
+import os
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from gpxfiles import *
-
-
-def haversine(originPair, destinationPair):
-    '''Great Circle Distance in terms of GPS coordinates.'''
-    lat1, lon1 = originPair
-    lat2, lon2 = destinationPair
-    earthRadius = 6371 
-
-    deltaLat = pd.np.radians(lat2-lat1)
-
-    deltaLon = pd.np.radians(lon2-lon1)
-    
-    alpha = pd.np.sin(deltaLat/2) * pd.np.sin(deltaLat/2) + pd.np.cos(pd.np.radians(lat1)) \
-        * pd.np.cos(pd.np.radians(lat2)) * pd.np.sin(deltaLon/2) * pd.np.sin(deltaLon/2)
-    beta = 2 * pd.np.arctan2(pd.np.sqrt(alpha), pd.np.sqrt(1-alpha))
-    
-    d = earthRadius * beta
-
-    return(d)
-
-
+from gpxfiles import GPXFile
 
 class HeatMap():
-    def __init__( self , path ):
-        self.path = path
-        self.files = [path + file for file in os.listdir(path)]
-        self.rawData = GPXFiles(self.files).points
-
-        self.data = self.build()
+    def __init__( self , data = pd.DataFrame() ):
+        self.data = data
+        self.dress()
         
-    def build( self ):
+    def sources( self ):
+        try:
+            output = self.data.source.unique()
+        except:
+            output = []
+        return(output)
         
-        data = self.rawData.copy()
+    def intake( self , file ):
         
-        # TODO : use this filename based data to create or check a taxonomy for runs
-        data["source"] = self.rawData.source.apply(lambda x : re.findall( "2020(.*)" , x)).apply(lambda x : "2020" + x[0])        
+        sources = self.sources()
+        if file not in sources:
+            data = GPXFile(file).points        
+            print( "processing " + file + "...")
+            self.data = pd.concat([self.data,data])
+            print("done.")
+            
+    def compilation( self , files ):
+        for file in sorted(files):
+            self.intake(file)
+            
+        self.dress()
         
-        # days
-        data["daysAgo"] = data["time"].apply(lambda x : dt.datetime.today() - x ).apply(lambda x : x.total_seconds()).apply(lambda x : x / 3600 / 24 ).apply(int)        
-
-        reference = list(data[["latitude","longitude"]].min())
-
-        data["east"] = data["longitude"].apply(lambda x : haversine([reference[0],x],reference))
-        data["north"] = data["latitude"].apply(lambda x : haversine([x,reference[1]],reference))
+    def export( self , path , dressed = False):
         
-        return(data)
+        output = self.data
+        if dressed:
+            output = self.dressed()
+        
+        self.data.to_csv( path , index = None)
 
-    def export( self ):
-        '''save the plots to file'''
-
-        figure = sns.scatterplot(x="longitude",y="latitude", data=self.data , hue="daysAgo" , edgecolor="none" , palette="Blues_r" , legend=None, sizes=(15, 15) )
-        figure.set_aspect(1)
-        figure.set_title("Trail Vlogging Heatmap: Lat Lon")
-        toShow = figure.get_figure()
-        figure.set_aspect(1)
-        toShow.savefig("ll_output.png")
-
-    def exportScaled( self ):
-        '''save the flattened/scaled plots to file.'''
-
-        figureScaled = sns.scatterplot(x="east",y="north", data=self.data , hue="daysAgo" , edgecolor="none" , palette="Blues_r" , legend=None, sizes=(15, 15) )
-        figureScaled.set_aspect(1)
-        figureScaled.set_title("Trail Vlogging Heatmap")
-        toShowScaled = figureScaled.get_figure()
-        figureScaled.set_aspect(1)
-        toShowScaled.savefig("km_output.png")
+    def dress( self ):
+        
+        metaData = lambda filename : dict(zip(["date","trailname","region"],filename.split("/")[-1].split(".")[0].split("_")))
+        output = self.data.join(pd.DataFrame(self.data.source.apply(metaData).tolist()))
+        output = output[[c for c in output.columns if c != "source"]]
+        
+        self.dressed = output
